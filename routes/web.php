f3c39482cd421c9479a9498e663d76e11899b407
Rainfog <muhammadzhafranbahij@gmail.com>
2024-05-07 11:04:27 +0700

    Feat: Landing Page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('landing-page');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+

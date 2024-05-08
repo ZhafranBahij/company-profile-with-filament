05526e2f89d4148b38d80c8672105edf97aef3ae
Rainfog <muhammadzhafranbahij@gmail.com>
2024-05-08 07:16:57 +0700

    FEAT: Add AboutUs, Article, Banner, Company, Faq, Partner, and Project in model and migration

---
+++ app/Models/AboutUs.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AboutUs extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function created_by()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function updated_by()
+    {
+        $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2024_05_07_235429_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('instagram_url')->nullable();
+            $table->string('facebook_url')->nullable();
+            $table->string('twitter_url')->nullable();
+            $table->string('linkedin_url')->nullable();
+            $table->string('youtube_url')->nullable();
+            $table->string('tiktok_url')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
